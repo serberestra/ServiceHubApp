972a131d8bf351cb3fc026150d290efa8a36a47b
serberestra <serberestra@gmail.com>
2020-06-05 16:45:36 -0400

    First Commit

---
+++ src/test/java/com/shub/app/ServiceHubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shub.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
